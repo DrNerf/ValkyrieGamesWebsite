200af48dc96051c83f33e0a082bd71e8224d47d0
Ivan <ivanivanov1992@gmail.com>
2015-04-26 18:28:16 +0200

    Login layout changes

--- PvPGamingWebsite/App_Start/AuthConfig.cs
+++ PvPGamingWebsite/App_Start/AuthConfig.cs
@@ -26,7 +26,7 @@ namespace PvPGamingWebsite
             //    appId: "",
             //    appSecret: "");
 
-            OAuthWebSecurity.RegisterGoogleClient();
+            //OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
